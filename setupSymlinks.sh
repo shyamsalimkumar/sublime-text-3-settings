0bdd9c60e612de891ed0d2f5f872993a551c26bf
Shyam S Kumar <shyam.salim.kumar@outlook.com>
2014-04-26 17:56:13 +0530

    Changed the folder structure based on https://sublime.wbond.net/docs/syncing

--- setupSymlinks.sh
+++ setupSymlinks.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 me=`basename $0`
-sublime_config_path="$HOME/.config/sublime-text-3/Packages/User"
+sublime3_config_path="$HOME/.config/sublime-text-3/Packages/User"
 
-
-for filename in *
-do
-    if [ "$filename" != "$me" ]; then
-        rm "$sublime_config_path/$filename"
-        ln -s "$PWD/$filename" "$sublime_config_path/$filename"
-    fi
-done
+rm -r "$sublime3_config_path"
+ln -s "$PWD/User" "$sublime3_config_path"
